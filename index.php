463e1346f1b8fa35f6366f62f9c858554674c880
taslimanaznineity27 <tnaznineity@gmail.com>
2021-11-21 15:06:27 +0600

    update sql

--- index.php
+++ index.php
@@ -2,10 +2,11 @@
 // its controllers
 require_once "Controllers/template.controller.php";
 require_once "Controllers/users.controller.php";
+require_once "Controllers/locations.controller.php";
 
 // its Models 
 require_once "Models/users.model.php";
-
+require_once "Models/locations.model.php";
 
 
 $template = new ControllersTemplate();
